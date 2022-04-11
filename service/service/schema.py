059f780d63b3cc1883836a20adf01f6d1c701e09
Serg <serg_myst@mail.ru>
2022-04-11 21:07:29 +0300

    Lesson 10

--- service/service/schema.py
+++ service/service/schema.py
@@ -18,6 +18,12 @@ class ProjectType(DjangoObjectType):
         fields = '__all__'
 
 
+class ToDoType(DjangoObjectType):
+    class Meta:
+        model = TodoNote
+        fields = '__all__'
+
+
 class Query(graphene.ObjectType):
     # Пример
     # hello = graphene.String(default_value="Hi!")
@@ -25,16 +31,50 @@ class Query(graphene.ObjectType):
 
     all_projects = graphene.List(ProjectType)
     all_users = graphene.List(UserType)
+    all_todo = graphene.List(ToDoType)
+
+    project_by_id = graphene.Field(ProjectType, id=graphene.String(required=True))
+
+    # Фильтрация с параметрами
+    def resolve_project_by_id(self, info, id):
+        try:
+            return Project.objects.get(id=id)
+        except Project.DoesNotExist:
+            return None
 
     def resolve_all_projects(root, info):
         return Project.objects.all()
+
+    def resolve_all_todo(root, info):
+        return TodoNote.objects.all()
 
     def resolve_all_users(root, info):
         return TodoUser.objects.all()
 
 
-schema = graphene.Schema(query=Query)
+# schema = graphene.Schema(query=Query)
 
+
+class ProjectMutation(graphene.Mutation):
+    class Arguments:
+        name = graphene.String(required=True)
+        id = graphene.ID()
+
+    project = graphene.Field(ProjectType)
+
+    @classmethod
+    def mutate(cls, root, info, name, id):
+        project = Project.objects.get(pk=id)
+        project.name = name
+        project.save()
+        return ProjectMutation(project=project)
+
+
+class Mutation(graphene.ObjectType):
+    update_project = ProjectMutation.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
 
 '''
 Пример запроса
@@ -63,6 +103,54 @@ newProjects: allProjects{
   }
 }
  
+# Фильтрация
+Запрос
+{
+	projectById(id: "737eddde-e197-4e2f-8f6d-f9367875d6d6") {
+	name
+	id
+	}
+}
+
+Ответ
+
+{
+  "data": {
+    "projectById": {
+      "name": "New",
+      "id": "737eddde-e197-4e2f-8f6d-f9367875d6d6"
+    }
+  }
+}
+
+# Обновление данных. В полнятиях графа Mutation
+
+Запрос:
+
+mutation updateProject {
+	updateProject(name: "Mutation", id: "737eddde-e197-4e2f-8f6d-f9367875d6d6") {
+		project {
+		id
+		name
+		}
+	}
+}
+
+Ответ:
+
+{
+  "data": {
+    "updateProject": {
+      "project": {
+        "id": "737eddde-e197-4e2f-8f6d-f9367875d6d6",
+        "name": "Mutation"
+      }
+    }
+  }
+}
+
+# Немного о микросервисах. На Ютубе
+Григорий Петров. Общение микросервисов: REST, JSON, GraphQL
 
 
 '''
